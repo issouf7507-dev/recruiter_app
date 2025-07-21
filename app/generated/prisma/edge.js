ed02a1c241450ecc1aa1a7ba40ca5c502990ae91
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2025-07-21 14:15:40 +0000

    plosmssssw

--- app/generated/prisma/edge.js
+++ app/generated/prisma/edge.js
@@ -773,11 +773,12 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "mysql://root:root0990@localhost:3306/db_recruter"
+        "value": null
       }
     }
   },
--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -774,11 +774,12 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "mysql://root:root0990@localhost:3306/db_recruter"
+        "value": null
       }
     }
   },
